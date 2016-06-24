0f7395ea75ee7605bc61d9f51bbe6affe9514949
蒼時弦也 <elct9620@frost.tw>
2016-06-24 21:36:49 +0800

    Let http client can call from anywhere

--- lib/line/bot/http.rb
+++ lib/line/bot/http.rb
@@ -3,20 +3,20 @@ require 'net/http'
 module Line
   module Bot
     class HTTP
-      def http(uri)
+      def self.http(uri)
         http = Net::HTTP.new(uri.host, uri.port)
         http.use_ssl = true if uri.scheme == "https"
         http
       end
 
-      def get(url, header = {})
+      def self.get(url, header = {})
         uri = URI(url)
-        http(uri).get(uri.requrest_uri, header)
+        self.http(uri).get(uri.requrest_uri, header)
       end
 
-      def post(url, payload, header = {})
+      def self.post(url, payload, header = {})
         uri = URI(url)
-        http(uri).post(uri.requrest_uri, payload, header)
+        self.http(uri).post(uri.requrest_uri, payload, header)
       end
     end
   end
--- spec/line/http_spec.rb
+++ spec/line/http_spec.rb
@@ -2,11 +2,9 @@ require 'spec_helper'
 
 describe Line::Bot::HTTP do
 
-  let(:http) { Line::Bot::HTTP.new }
-
   it 'should enable SSL for http client' do
     uri = URI('https://example.com/')
-    expect(http.http(uri)).to be_use_ssl
+    expect(Line::Bot::HTTP.http(uri)).to be_use_ssl
   end
 
   it 'should get data from remote server'
